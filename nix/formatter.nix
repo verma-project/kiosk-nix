20a7671577def31b4d6b07d832485a71d6a138b9
Dom Rodriguez <shymega@shymega.org.uk>
2024-11-23 22:09:52 +0000

    feat: Add formatting checks to Nix Flake
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ nix/formatter.nix
@@ -0,0 +1,29 @@
+# SPDX-FileCopyrightText: 2024 Dom Rodriguez <shymega@shymega.org.uk>
+#
+# SPDX-License-Identifier: GPL-3.0-only
+{pkgs, ...}: {
+  package = pkgs.treefmt;
+  projectRootFile = "flake.nix";
+
+  settings = {
+    global.excludes = [
+      "*.age"
+      "*.md"
+      "*.gpg"
+      "*.bin"
+    ];
+    shellcheck.includes = [
+      "*"
+      ".envrc"
+    ];
+  };
+  programs = {
+    deadnix.enable = true;
+    statix.enable = true;
+    alejandra.enable = true;
+    yamlfmt.enable = true;
+    jsonfmt.enable = true;
+    mdformat.enable = true;
+    actionlint.enable = true;
+  };
+}
