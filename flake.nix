b0c7e54139294580ae639c0cf0bfd06aa3ca6404
Dom Rodriguez <shymega@shymega.org.uk>
2024-11-23 22:10:34 +0000

    chore: Run formatter on source files
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- flake.nix
+++ flake.nix
@@ -22,8 +22,8 @@
   };
 
   nixConfig = {
-    extra-trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
-    extra-trusted-substituters = [ "https://cache.nixos.org/" ];
+    extra-trusted-public-keys = ["cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="];
+    extra-trusted-substituters = ["https://cache.nixos.org/"];
   };
 
   outputs = {self, ...} @ inputs: let
