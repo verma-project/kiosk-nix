9a24e4f0cc4c697c363a61da98751c66cd4783d6
Dom Rodriguez <shymega@shymega.org.uk>
2024-12-03 21:21:33 +0000

    chore: Remove `rec` attr for flake.inputs
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
 {
   description = "A very basic flake";
 
-  inputs = rec {
+  inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
