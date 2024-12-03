119dd1cc9ce395ec5aed6459802cda38440cf122
Dom Rodriguez <shymega@shymega.org.uk>
2024-12-03 21:20:15 +0000

    chore: Run Alejandra on sources
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- packages/default.nix
+++ packages/default.nix
@@ -1,7 +1,11 @@
 # SPDX-FileCopyrightText: 2024 Dom Rodriguez <shymega@shymega.org.uk>
 #
 # SPDX-License-Identifier: GPL-3.0-only
-{pkgs, ...}:
+{
+  pkgs,
+  self,
+  ...
+}:
 with pkgs; {
   libcec-daemon = callPackage ./libcec-daemon {};
 }
